6eb4c6c5851a60f21b52c08b1bf99781e1b7a6b2
Scott Willson <scott.willson@gmail.com>
2020-03-29 10:29:04 -0700

    Add bunch of widths

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,68 @@
 Question.destroy_all
 Answer.destroy_all
 
+nothing = Answer.create!(text: "Nothing")
 fingers = Answer.create!(text: "Fingers")
 hands = Answer.create!(text: "Hands")
 fists = Answer.create!(text: "Fists")
 off_width = Answer.create!(text: "Off-width")
 
-question = Question.create!(text: "0.5\" wide crack upper body size")
-question.answers << fingers
-question.answers << hands
-question.answers << fists
-question.answers << off_width
-question.answer = fingers
+((0.0)..(0.45)).step(0.05).each do |size|
+  size = size.round(2)
+  question = Question.create!(text: "#{size}\" wide crack upper body size")
+  question.answers << fingers
+  question.answers << hands
+  question.answers << fists
+  question.answers << off_width
+  question.answer = nothing
+end
 
-question = Question.create!(text: "1.35\" wide crack upper body size")
-question.answers << fingers
-question.answers << hands
-question.answers << fists
-question.answers << off_width
-question.answer = hands
+((0.50)..(1.30)).step(0.05).each do |size|
+  size = size.round(2)
+  question = Question.create!(text: "#{size}\" wide crack upper body size")
+  question.answers << fingers
+  question.answers << hands
+  question.answers << fists
+  question.answers << off_width
+  question.answer = fingers
+end
 
-question = Question.create!(text: "2.5\" wide crack upper body size")
-question.answers << fingers
-question.answers << hands
-question.answers << fists
-question.answers << off_width
-question.answer = fists
+((1.35)..(2.45)).step(0.05).each do |size|
+  size = size.round(2)
+  question = Question.create!(text: "#{size}\" wide crack upper body size")
+  question.answers << fingers
+  question.answers << hands
+  question.answers << fists
+  question.answers << off_width
+  question.answer = hands
+end
 
-question = Question.create!(text: "3.75\" wide crack upper body size")
-question.answers << fingers
-question.answers << hands
-question.answers << fists
-question.answers << off_width
-question.answer = off_width
+((2.5)..(3.7)).step(0.05).each do |size|
+  size = size.round(2)
+  question = Question.create!(text: "#{size}\" wide crack upper body size")
+  question.answers << fingers
+  question.answers << hands
+  question.answers << fists
+  question.answers << off_width
+  question.answer = fists
+end
+
+((3.75)..(4)).step(0.05).each do |size|
+  size = size.round(2)
+  question = Question.create!(text: "#{size}\" wide crack upper body size")
+  question.answers << fingers
+  question.answers << hands
+  question.answers << fists
+  question.answers << off_width
+  question.answer = off_width
+end
+
+(5..12).each do |size|
+  size = size.round(2)
+  question = Question.create!(text: "#{size}\" wide crack upper body size")
+  question.answers << fingers
+  question.answers << hands
+  question.answers << fists
+  question.answers << off_width
+  question.answer = off_width
+end
