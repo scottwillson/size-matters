9b0b2ec1364d29489b97a1a4f06f84b84c15fd41
Scott Willson <scott.willson@gmail.com>
2020-03-29 09:18:04 -0700

    Stub out question

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,4 +1,10 @@
 class QuizzesController < ApplicationController
   def index
+    @answers = [
+      Answer.new(text: "Fingers"),
+      Answer.new(text: "Hands"),
+      Answer.new(text: "Fists"),
+      Answer.new(text: "Off-width")
+    ]
   end
 end
