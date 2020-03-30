f372bdca8a6e799fa94184df7617a5b14ff0160b
Scott Willson <scott.willson@gmail.com>
2020-03-29 18:43:24 -0700

    Correctly check multiple answers

--- app/controllers/quiz_answers_controller.rb
+++ app/controllers/quiz_answers_controller.rb
@@ -1,12 +1,12 @@
 class QuizAnswersController < ApplicationController
   def create
-    @answers = Choice.find(quiz_answer_params[:answer_id])
+    @answers = Choice.find(quiz_answer_params[:choice_id].reject(&:blank?))
     @question = Question.find(params[:question_id])
   end
 
   private
 
   def quiz_answer_params
-    params.require(:quiz_answer).permit(answer_id: [])
+    params.require(:quiz_answer).permit(choice_id: [])
   end
 end
